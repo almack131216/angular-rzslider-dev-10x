08e7f192ae989b1438849091a99191d69f62e770
ecp-alex <alex.mackenzie@englishcollege.cz>
2018-02-07 19:04:01 +0100

    r2 | advice + styling bug fix

--- demo/demo.js
+++ demo/demo.js
@@ -14,19 +14,21 @@ app.controller('MainCtrl', function ($scope, $rootScope, $timeout, $uibModal) {
       ceil: 120,
       step: 1,
       showSelectionBar: false,
-      showAdvice: 10,
+      showAdvice: 100,
       powerOutput:0.2565,
       finalValue: 7,
       optionBtnPlus: function () {
         console.log('!!! amcust: options > optionBtnPlus() [' + $scope.amcustDebugSlider.value + ' -> ' + ($scope.amcustDebugSlider.value + 1) + ']');
         if ($scope.amcustDebugSlider.value < $scope.amcustDebugSlider.options.ceil) {
-          $scope.amcustDebugSlider.value++;
+          $scope.amcustDebugSlider.value = $scope.amcustDebugSlider.value + 10;
+          // $scope.amcustDebugSlider.value = ($scope.amcustDebugSlider.options.finalValue + 1) * 10;
         }
       },
       optionBtnMinus: function () {
         console.log('!!! amcust: options > optionBtnMinus() [' + $scope.amcustDebugSlider.value + ' -> ' + ($scope.amcustDebugSlider.value - 1) + ']');
         if (($scope.amcustDebugSlider.value > $scope.amcustDebugSlider.options.floor) && $scope.amcustDebugSlider.options.floor > 1) {
-          $scope.amcustDebugSlider.value--;
+          $scope.amcustDebugSlider.value = $scope.amcustDebugSlider.value - 10;
+          // $scope.amcustDebugSlider.value = ($scope.amcustDebugSlider.options.finalValue - 1) * 10;
         }
       }
     },
